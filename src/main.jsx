ea070a7f660035f09d74a5978d86e6d18e1acc8a
Pragati246 <pragatitomar246@gmail.com>
2023-07-30 17:14:35 +0530

    feat:alert utility

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import './App.css'
 import {BrowserRouter} from 'react-router-dom'
+import { SnackbarProvider } from 'notistack' 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <SnackbarProvider>
     <App />
+    </SnackbarProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
